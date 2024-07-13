8dc39e8d3c258d5787c2a1f04f31d79e014ba815
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-07-12 21:10:20 -0300

    feat: adds round-robin and rinha-account balancers using arc

--- load_balancer/src/main.rs
+++ load_balancer/src/main.rs
@@ -12,25 +12,78 @@ use hyper_util::{
     client::legacy::{connect::HttpConnector, Client},
     rt::TokioExecutor,
 };
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
+use std::{
+    hash::{DefaultHasher, Hash, Hasher},
+    str::FromStr,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
 use tokio::net::TcpListener;
 
 #[derive(Clone)]
 struct AppState {
-    addresses: Vec<&'static str>,
     http_client: Client<HttpConnector, Body>,
+    load_balancer: Arc<dyn LoadBalancer + Send + Sync>,
+}
+
+trait LoadBalancer {
+    fn next_server(&self, request: &Request) -> String;
+}
+
+struct RoundRobinBalancer {
+    addresses: Vec<&'static str>,
     request_counter: Arc<AtomicUsize>,
+}
+
+impl LoadBalancer for RoundRobinBalancer {
+    fn next_server(&self, _: &Request) -> String {
+        let count = self.request_counter.fetch_add(1, Ordering::Relaxed);
+
+        self.addresses[count % self.addresses.len()].to_string()
+    }
+}
+
+struct RinhaAccountBalancer {
+    addresses: Vec<&'static str>,
+}
+
+impl LoadBalancer for RinhaAccountBalancer {
+    fn next_server(&self, request: &Request) -> String {
+        let path = request.uri().path();
+        let hash = {
+            let mut hasher = DefaultHasher::new();
+
+            path.hash(&mut hasher);
+
+            hasher.finish() as usize
+        };
+
+        self.addresses[hash % self.addresses.len()].to_string()
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:9999").await.unwrap();
+    let http_client = Client::builder(TokioExecutor::new()).build_http::<Body>();
+    let addresses = ["0.0.0.0:9997", "0.0.0.0:9998"];
+
+    #[allow(unused)]
+    let round_robin = RoundRobinBalancer {
+        addresses: addresses.to_vec(),
+        request_counter: Arc::new(AtomicUsize::new(0)),
+    };
+
+    #[allow(unused)]
+    let rinha_account = RinhaAccountBalancer {
+        addresses: addresses.to_vec(),
+    };
 
     let app_state = AppState {
-        addresses: ["0.0.0.0:9997", "0.0.0.0:9998"].to_vec(),
-        http_client: Client::builder(TokioExecutor::new()).build_http::<Body>(),
-        request_counter: Arc::new(AtomicUsize::new(0)),
+        http_client,
+        load_balancer: Arc::new(round_robin),
     };
 
     let app = proxy.with_state(app_state);
@@ -40,19 +93,18 @@ async fn main() {
 
 async fn proxy(
     State(AppState {
-        addresses,
         http_client,
-        request_counter,
+        load_balancer,
     }): State<AppState>,
     mut request: Request,
 ) -> impl IntoResponse {
-    let count = request_counter.fetch_add(1, Ordering::Relaxed);
+    let address = load_balancer.next_server(&request);
 
     *request.uri_mut() = {
         let uri = request.uri();
         let mut parts = uri.clone().into_parts();
 
-        parts.authority = Some(Authority::from_static(addresses[count % addresses.len()]));
+        parts.authority = Authority::from_str(address.as_str()).ok();
         parts.scheme = Some(Scheme::HTTP);
 
         Uri::from_parts(parts).unwrap()
